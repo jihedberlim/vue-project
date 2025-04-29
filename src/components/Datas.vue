8eed05d2507e505e4099a35df0f1f9f1687cef07
Jihed Berlim <jihedberlim@gmail.com>
2025-04-29 11:54:43 -0300

    feat: using options API and composition API

---
+++ src/components/Datas.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="component">
+        <h1>Data Components</h1>
+        <p>Name: {{ nameProduct }} - {{ codProduct }}</p>
+    </div>
+</template>
+
+<script setup>
+    const nameProduct = "Book"
+    const codProduct = "#987"
+</script>
+
+<!-- <script>
+    //options API - antigamente utilizada no Vue2
+    // export default {
+    //     data: function() {
+    //         return {
+    //             nameProduct: "Pencil",
+    //             codProduct: "#321"
+    //         }
+    //     }
+    // }
+
+    // composition API - utilizado a partir do Vue3
+    // export default {
+    //     setup:function() {
+    //         const nameProduct = "Pen"
+    //         const codProduct = "#789"
+    //         return {nameProduct, codProduct}
+    //     }
+    // }
+</script> -->
