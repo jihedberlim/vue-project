f17e600229fd9122de235d0abed6da7e9a45be08
Jihed Berlim <jihedberlim@gmail.com>
2025-04-29 14:05:48 -0300

    feat: using props in other components

---
+++ src/components/WithProps.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="component">
+        <h1>Component with Props</h1>
+        <p>{{ item }} (R${{ price.toFixed(2) }})</p>
+    </div>
+</template>
+
+<script setup>
+    import { defineProps } from 'vue';
+
+    defineProps({
+        item: String,
+        price: Number
+    })
+</script>
