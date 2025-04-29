900c4c92c94d0e9d0ecfa7dcab0532db50fb35d0
Jihed Berlim <jihedberlim@gmail.com>
2025-04-29 13:25:29 -0300

    feat: presentation of values ​​in input with data binding

---
+++ src/components/DataBinding.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="component">
+        <h1>Data Binding Component</h1>
+        <p>{{ valueText }}</p>
+        <!-- <input type="text" v-bind:value="valueText"> one way Data Binding -->
+        <!-- <input type="text" :value="valueText" @input="(event)=>valueText = event.target.value"> two way Data Binding -->
+        <input type="text" v-model="valueText"> <!-- two way Data Binding simple -->
+    </div>
+</template>
+
+<script setup>
+    import {ref} from "vue"
+
+    const valueText = ref("Vue is so cool!")
+</script>
