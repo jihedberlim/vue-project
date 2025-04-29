10a7c07b5ee1b4e46cf75bcb24466fe701379e33
Jihed Berlim <jihedberlim@gmail.com>
2025-04-29 11:43:42 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+  import FirstComponent from "./components/FirstComponent.vue";
+  import Datas from "./components/Datas.vue";
+  import PrimitiveReactiveData from "./components/PrimitiveReactiveData.vue";
+  import NonPrimitiveReactiveData from "./components/NonPrimitiveReactiveData.vue";
+  import DataBinding from "./components/DataBinding.vue";
+  import WithProps from "./components/WithProps.vue";
+</script>
+
+<template>
+  <header>Vuejs</header>
+  <WithProps item="Eraser" :price="2.50"/>
+  <DataBinding />
+  <NonPrimitiveReactiveData />
+  <PrimitiveReactiveData />
+  <Datas/>
+  <FirstComponent/>
+
+</template>
---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>First Component</h1>
+    </div>
+</template>
+
+<style scoped>
+    div {
+        background-color: #42b883;
+        padding: 20px;
+        margin: 30px;
+        border-radius: 5px;
+        text-align: center;
+    }
+
+    div:hover {
+        background-color: #3ed390;
+    }
+
+    h1 {
+        color: white;
+        margin-bottom: 10px;
+    }
+</style>
