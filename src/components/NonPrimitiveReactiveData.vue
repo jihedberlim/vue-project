28424e579112f2037a38bfe84e27feaea7843b80
Jihed Berlim <jihedberlim@gmail.com>
2025-04-29 13:19:21 -0300

    feat: use primitive and non-primitive data to get values

---
+++ src/components/NonPrimitiveReactiveData.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="component">
+        <h1>Non-Primitive Reactive Data</h1>
+        <p>Name: {{ person.name }} | Age: {{ person.age }}</p>
+        <button @click="changeName()">Change Name</button>
+        <button @click="changeAge()">Change Age</button>
+    </div>
+</template>
+
+<!-- <script setup>
+    import {reactive} from "vue"
+
+    const person = reactive({name: "Elmograbi", age: 29})
+
+    function changeName() {
+        person.name = "Berlim"
+    }
+
+    function changeAge() {
+        person.age += 1
+    }
+</script> -->
+
+<script>
+    export default{
+        data:function() {
+            return {
+                person: {
+                    name: "Jihed",
+                    age: 29
+                }
+            }
+        },
+        methods:{
+            changeAge:function() {
+                this.person.age +=1
+            },
+            changeName:function() {
+                this.person.name = "Jihed Berlim"
+            }
+        }
+    }
+</script>
---
+++ src/components/PrimitiveReactiveData.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="component">
+        <h1>Primitive Reactive Data</h1>
+        <p>Name: {{ name }} | Age: {{ age }}</p>
+        <button v-on:click="changeName()">Change Name</button>
+        <button @click="changeAge()">Change Age</button>
+    </div>
+</template>
+
+<script setup>
+    import {ref} from "vue"
+    const name = ref("Jihed")
+    const age = ref(28)
+
+    function changeName() {
+        name.value = "Berlim"
+    }
+
+    function changeAge() {
+        age.value += 1
+    }
+</script>
