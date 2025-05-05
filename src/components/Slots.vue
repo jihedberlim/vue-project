885b07d85c70acf15355acd642b996d649a2357a
Jihed Berlim <jihedberlim@gmail.com>
2025-05-05 11:52:38 -0300

    feat: create slots component to use items through properties

---
+++ src/components/Slots.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="component">
+        <h1>Slots Component</h1>
+        <button>
+            <slot name="icon"/> 
+            <slot name="text"/>
+        </button>
+    </div>
+</template>
